f64e0cc22c3295034b468fe4139407954ef7d067
Mike Schinkel <mike@newclarity.net>
2012-03-23 18:08:35 -0400

    First commit

---
+++ imperative.php
@@ -0,0 +1,13 @@
+<?php
+/**
+ * Library Registration and Loader for WordPress
+ *
+ * @package Imperative
+ * @version 0.1
+ * @author Mike Schinkel <mike@newclarity.net>
+ * @author Micah Wood <micah@newclarity.net>
+ * @license http://opensource.org/licenses/gpl-2.0.php
+ * @copyright Copyright (c) 2012, NewClarity LLC
+ *
+ */
+define( 'IMPERATIVE_LIB', true );
